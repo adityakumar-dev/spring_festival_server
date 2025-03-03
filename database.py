7b675463b976676d02f9e1280e4372fd1199bbf6
adityakumar-dev <adityakumar.devxlinux@gmail.com>
2025-03-03 18:58:55 +0530

    1

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, pool
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Correcting the DATABASE_URL with the password included
 DATABASE_URL = "postgresql://admin:yourpassword@localhost:5432/guestdb"
 
-# Creating the SQLAlchemy engine
-engine = create_engine(DATABASE_URL)
+# Creating the SQLAlchemy engine with a connection pool for multithreading
+engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0)
 
 # Session local to bind the engine for transactions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
